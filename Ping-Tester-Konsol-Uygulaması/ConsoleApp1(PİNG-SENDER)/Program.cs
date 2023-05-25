60e8a607a57c2139838a7cc83ef62366d80657b7
Efe Aygün <82315578+ROOTMEA@users.noreply.github.com>
2023-05-25 15:25:06 +0300

    Add files via upload

---
+++ Ping-Tester-Konsol-Uygulaması/ConsoleApp1(PİNG-SENDER)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+namespace ConsoleApp1_PİNG_SENDER_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ping ping = new Ping();
+            Console.WriteLine("IP Adresi Girin:");
+            PingReply reply = ping.Send(Console.ReadLine());
+            Console.WriteLine(reply.Status.ToString());
+            Console.ReadKey();
+
+        }
+    }
+}
